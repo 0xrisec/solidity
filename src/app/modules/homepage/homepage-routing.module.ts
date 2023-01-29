1d6287a8130d6508bc46bae6eab6f6d67b328c34
ROOT BABU <babuhacker360@gmail.com>
2023-01-29 15:31:24 +0530

    added coming soon page

--- src/app/modules/homepage/homepage-routing.module.ts
+++ src/app/modules/homepage/homepage-routing.module.ts
@@ -5,7 +5,7 @@ import { DocLayoutComponent } from 'src/app/components/doc/doclayout.component';
 import { HomepageComponent } from 'src/app/components/homepage/homepage.component';
 import { RoadmapsComponent } from 'src/app/components/roadmaps/roadmaps.component';
 import basicsConfig from 'src/assets/config/basics.json';
-import bugsConfig from 'src/assets/config/bugs.json';
+import hacksConfig from 'src/assets/config/hacks.json';
 import homeConfig from 'src/assets/config/homepage.json';
 import roadmapsConfig from 'src/assets/config/roadmaps.json'
 
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path:'basics/:fileName',component:DocLayoutComponent,data: basicsConfig},
   {path:'blogs',component:BlogsComponent},
   {path:'roadmaps',component:RoadmapsComponent,data: roadmapsConfig},
-  {path:'bugs',component:DocLayoutComponent,data: bugsConfig}
+  {path:'hacks/:fileName',component:DocLayoutComponent,data: hacksConfig}
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
